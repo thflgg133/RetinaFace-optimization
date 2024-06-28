f92b5edcc265beecd93f4e79f1ab07d8ec11c412
thflgg133 <thflgg133@gmail.com>
2024-06-28 11:46:29 +0900

    RetinaFace Optimization

---
+++ data/__init__.py
@@ -0,0 +1,5 @@
+from .wider_face import WiderFaceDetection, detection_collate
+# from .custom_wider_face import WiderFaceDetection, detection_collate
+from .data_augment import *
+# from .custom_data_augment import *
+from .config import *
